8cbba166e89b44fa31d14b3bdd8cde064c9e9da3
Ali Hesari <alihesarim@gmail.com>
2026-02-21 02:57:31 +0100

    fix: update author information and homepage URL in plugin files

--- owlstack.php
+++ owlstack.php
@@ -7,8 +7,8 @@
  * Version:           1.0.0
  * Requires at least: 6.4
  * Requires PHP:      8.1
- * Author:            Ali Hesari
- * Author URI:        https://alihesari.com
+ * Author:            Owlstack
+ * Author URI:        https://owlstack.dev
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       owlstack
